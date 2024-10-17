e123c4541df8050c5e02cde59d552fab8d9c645e
Gabin <159160837+Kartyxx@users.noreply.github.com>
2024-10-17 11:52:33 +0200

    Update connexion.php

--- src/connexion.php
+++ src/connexion.php
@@ -34,17 +34,19 @@ else{
 
 
 
-<div>
-<form action="connexion.php" method="post" class="space-y-4 font-[sans-serif] max-w-md mx-auto">
-    <input type="email" name="email" placeholder="Votre id" class="px-4 py-3 bg-gray-100 w-full text-sm outline-none border-b-2 border-blue-500 rounded" required>
-    <input type="password" name="mdp" placeholder="Entrez un mdp" class="px-4 py-3 bg-gray-100 w-full text-sm outline-none border-b-2 border-transparent focus:border-blue-500 rounded" required>
-    <button id="" type="submit" class="!mt-8 w-full px-4 py-2.5 mx-auto block text-sm bg-blue-500 text-white rounded hover:bg-blue-600">Se connecter</button>
-</form>
+<div class="min-h-screen flex items-center justify-center bg-gray-900">
+  <form action="connexion.php" method="post" class="bg-gray-700 p-8 rounded-lg shadow-lg text-orange-50 space-y-4 max-w-md w-full">
+    <input type="email" name="email" placeholder="Votre id" class="px-4 py-3 bg-slate-950 w-full text-sm outline-none border-b-2 border-amber-500 rounded" required>
+    <input type="password" name="mdp" placeholder="Entrez un mdp" class="px-4 py-3 bg-slate-950 w-full text-sm outline-none border-b-2 border-amber-500 rounded" required>
+    <button id="" type="submit" class="!mt-8 w-full px-4 py-2.5 mx-auto block text-sm bg-amber-500 text-white rounded hover:bg-amber-700">Se connecter</button>
+    <a href="inscription.php" class="my-5">Inscription</a>
+  </form>
+  
 </div>
 
 
 
-    <a href="inscription.php" class="">Inscription</a>
+
 
 
     <?php 
